7fbf4d554a7807cefa1b9ba5e59486b9631053cb
komalbankar2001 <141000764+komalbankar2001@users.noreply.github.com>
2024-05-28 16:05:09 +0530

    Update __manifest__.py

--- pragtech_onedrive_integration/__manifest__.py
+++ pragtech_onedrive_integration/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '14.0.2',
     'category': 'Services',
     'author': 'Pragmatic TechSoft Pvt Ltd.',
-    'website': 'http://www.pragtech',
+    'website': 'https://chatgpt.com',
     'summary': 'Odoo OneDrive Integration odoo app odoo onedrive integration odoo onedrive connector odoo onedrive app',
     'description': """
 Odoo OneDrive Integration
